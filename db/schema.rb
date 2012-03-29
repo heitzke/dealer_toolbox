89851290013c4e45969aae0358b5f6cb82005c72
Robby Clements <rclements@isotope11.com>
2012-03-28 19:06:18 -0500

    weird merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120323004651) do
+ActiveRecord::Schema.define(:version => 20120327002100) do
 
   create_table "categories", :force => true do |t|
     t.string "name"
@@ -32,5 +32,25 @@ ActiveRecord::Schema.define(:version => 20120323004651) do
     t.string "description"
     t.string "category_id"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "username",               :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
+    t.boolean  "admin",                  :default => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
