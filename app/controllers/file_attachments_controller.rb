8a6fe38cf900558f91f10abb42c3b07fdc5148fc
Robby Clements <rclements@isotope11.com>
2012-04-13 08:07:14 -0500

    added destroy capability to files

--- app/controllers/file_attachments_controller.rb
+++ app/controllers/file_attachments_controller.rb
@@ -24,4 +24,23 @@ class FileAttachmentsController < ApplicationController
     send_file(@file_attachment.attachment_file.path, :disposition => 'attachment')
   end
 
+  def index
+    @subcategory = Subcategory.find(params[:subcategory_id])
+    @file_attachments = @subcategory.file_attachments
+    @file_attachments = @file_attachments.paginate(:per_page => 20, :page => params[:page])
+  end
+
+  def destroy
+    @file_attachment = FileAttachment.find(params[:id])
+    @subcategory = Subcategory.find(@file_attachment.subcategory_id)
+    if @file_attachment.destroy
+      flash[:notice] = "File Deleted!"
+      redirect_to category_subcategory_path(@subcategory.category, @subcategory)
+    else
+      flash[:error] = "File Not Deleted!"
+      render :action => 'index'
+    end
+
+  end
+
 end
